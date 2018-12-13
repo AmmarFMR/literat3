5da99911a96ccccd938e29d5e077e707410fe0de
Ammar Farhan Sarif <ammarfmr11@gmail.com>
2018-12-14 01:42:43 +0800

    Committing 1.0 alpha

---
+++ literat3/secret_key.py
@@ -0,0 +1,4 @@
+encryption_key = '004^b8p4-(!$55%3ez=yul4v5p0m76md0e_6%b1n5%g(nscsnz'
+
+def get_secret_key():
+    return encryption_key
---
+++ posts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import Posts
+# from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    # return HttpResponse('Hello from Posts!')
+    posts = Posts.objects.all()[:10:-1]
+    context = {
+        'title': 'Latest Posts',
+        'posts': posts
+    }
+    return render(request, 'posts/index.html', context)
+
+def details(request, id):
+    post = Posts.objects.get(id=id)
+    context = {'post': post}
+    return render(request, 'posts/details.html', context)
