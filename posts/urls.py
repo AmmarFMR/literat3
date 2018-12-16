6defe2791e6c5262fa271d74cb677c7f6c622a8c
Ammar Farhan Sarif <ammarfmr11@gmail.com>
2018-12-17 04:25:22 +0800

    Added more files

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^details/(?P<id>\d+)/$', views.details, name='details')
+    url(r'^$', views.posts, name='posts'),
+    url(r'^details/(?P<id>\d+)/$', views.details, name='details'),
+    url(r'^user_details/(?P<id>\d+)/$', views.user_details, name='user_details')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Posts
+from django.contrib.auth.models import User
 # from django.http import HttpResponse
 
 # Create your views here.
@@ -11,9 +12,18 @@ def index(request):
         'title': 'Latest Posts',
         'posts': posts
     }
-    return render(request, 'posts/index.html', context)
+    return render(request, 'posts/posts.html', context)
 
 def details(request, id):
     post = Posts.objects.get(id=id)
     context = {'post': post}
     return render(request, 'posts/details.html', context)
+
+def user_details(request, id):
+    user = User.objects.filter(id=id).first()
+    posts = user.posts_set.all()[:10]
+    context = {
+        'title': f'Posts by {user.username}',
+        'posts': posts
+    }
+    return render(request, 'posts/user_details.html', context)
