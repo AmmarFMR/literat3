6163e130e9f233d9a099e84d89e009e5b1f38b94
Ammar Farhan Sarif <ammarfmr11@gmail.com>
2018-12-18 08:24:04 +0800

    Added Posts list view

---
+++ frontend/gui/src/containers/PostsListView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Posts from '../components/Posts'
+
+class PostsList extends React.Component {
+    render() {
+        return (
+            <Posts />
+        );
+    }
+}
+
+export default PostsList;
